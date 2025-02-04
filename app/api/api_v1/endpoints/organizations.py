a033d7a783420743bd0a989371626c2ef6d6bc94
dssera <adesser001@mail.ru>
2025-02-04 18:54:34 +0300

    refactor/integrated splitted deps in controllers

--- app/api/api_v1/endpoints/organizations.py
+++ app/api/api_v1/endpoints/organizations.py
@@ -8,12 +8,12 @@ from sqlalchemy.orm import Session, relationship
 
 from app import services, schemas
 from app.dependencies import organization_service
-from app.services import OrganizationService
-
+from app.services import OrganizationService, BuildingService
 
 router = APIRouter()
 
-service_dep = Annotated[OrganizationService, Depends(organization_service)]
+organization_service_dep = Annotated[OrganizationService, Depends(organization_service)]
+building_service_dep = Annotated[BuildingService, Depends(organization_service)]
 
 
 @router.get("/get_organizations_by_building_address",
@@ -22,7 +22,7 @@ def get_organizations_by_building_address(
         city: str,
         street: str,
         house: str,
-        service: service_dep
+        service: organization_service_dep
 ) -> List[schemas.Organization]:
     organizations = service.get_organizations_by_building_address(city, street, house)
     if not organizations:
@@ -34,7 +34,7 @@ def get_organizations_by_building_address(
             response_model=List[schemas.Organization])
 def get_organizations_by_activity(
         activity: str,
-        service: service_dep
+        service: organization_service_dep
 ) -> List[schemas.Organization]:
     organizations = service.get_organizations_by_activity(activity)
     if not organizations:
@@ -46,7 +46,7 @@ def get_organizations_by_activity(
             response_model=schemas.Organization)
 def get_organization_by_id(
         organization_id: int,
-        service: service_dep
+        service: organization_service_dep
 ) -> schemas.Organization | None:
     organization = service.get_organization_by_id(organization_id)
     if not organization:
@@ -58,12 +58,13 @@ def get_organization_by_id(
             response_model=schemas.Organization)
 def get_organization_by_name(
         name: str,
-        service: service_dep
+        service: organization_service_dep
 ) -> schemas.Organization | None:
     organization = service.get_organization_by_name(name)
     if not organization:
         raise HTTPException(404, "No data by this query")
     return organization
+
 
 @router.get("/get_organizations_by_coordinates",
             response_model=List[dict])
@@ -71,23 +72,21 @@ def get_organizations_by_coordinates(
         latitude: float,
         longitude: float,
         radius: float,
-        service: service_dep
+        service: building_service_dep
 ) -> List[dict]:
     buildings_organizations_dicts: list = service.get_by_coordinates(latitude, longitude, radius)
     if not buildings_organizations_dicts:
         raise HTTPException(404, "No data by this query")
     return buildings_organizations_dicts
 
+
 @router.get("/get_organizations_by_subactivities",
             response_model=List[schemas.Organization])
 def get_organizations_by_subactivities(
         activity: str,
-        service: service_dep
+        service: organization_service_dep
 ):
     organizations = service.get_organizations_by_subactivities(activity)
     if not organizations:
         raise HTTPException(404, "No data by this query")
     return organizations
-
-
-
