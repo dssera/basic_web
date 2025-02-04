c4eba6821914a154d2989efca97bd99939dac9ec
dssera <adesser001@mail.ru>
2025-02-04 18:43:38 +0300

    refactor/split OrganizationService into OrganizationService, BuildingService and GeoUtils

--- app/services.py
+++ app/services.py
@@ -1,14 +1,38 @@
 from geopy import Nominatim
 from geopy.distance import distance
 
-from .organization_repository import OrganizationRepository
+from .repositories import OrganizationRepository, BuildingRepository, ActivityRepository
+
+
+class BuildingService:
+    def __init__(self,
+                 building_repo: BuildingRepository,
+                 organization_repo: OrganizationRepository):
+        self.building_repo = building_repo
+        self.organization_repo = organization_repo
+
+    def get_by_coordinates(self, latitude: float, longitude: float, r: float):
+        # shitty method name btw: get_buildings_with_organizations_by_coordinates
+        city = GeoUtils.find_city_by_coordinates(latitude, longitude)
+        if not city:
+            return None
+        buildings = self.building_repo.get_buildings_by_city(city)
+        result_list = []
+        for b in buildings:
+            if GeoUtils.is_within_radius(latitude, longitude, b.latitude, b.longitude, r):
+                building_dict = b.model_dump()
+                building_dict["organizations"] = (
+                    self.organization_repo.get_organizations_by_building_address(b.city, b.street, b.house))
+                result_list.append(building_dict)
+        return result_list
 
 
 class OrganizationService:
 
-    # must depend on abs class actually
-    def __init__(self, organization_repo: OrganizationRepository):
-        self.repo = organization_repo
+    def __init__(self, organization_repo: OrganizationRepository,
+                 activity_repo: ActivityRepository):
+        self.organization_repo = organization_repo
+        self.activity_repo = activity_repo
 
     def get_organizations_by_building_address(
             self,
@@ -16,13 +40,26 @@ class OrganizationService:
             street: str,
             house: str
     ):
-        return self.repo.get_organizations_by_building_address(city, street, house)
+        return self.organization_repo.get_organizations_by_building_address(city, street, house)
 
     def get_organizations_by_activity(self, activity: str):
-        return self.repo.get_organizations_by_activity(activity)
+        return self.organization_repo.get_organizations_by_activity(activity)
 
+    def get_organization_by_id(self, organization_id: int):
+        return self.organization_repo.get_organization_by_id(organization_id)
+
+    def get_organization_by_name(self, name: str):
+        return self.organization_repo.get_organization_by_name(name)
+
+    def get_organizations_by_subactivities(self, activity: str):
+        subactivities = self.activity_repo.get_all_subactivities(activity)
+        return self.organization_repo.find_organizations_by_activity(activity, subactivities)
+
+
+class GeoUtils:
+
+    @staticmethod
     def is_within_radius(
-            self,
             input_point_latitude,
             input_point_longitude,
             obj_latitude,
@@ -34,7 +71,8 @@ class OrganizationService:
                                (obj_latitude, obj_longitude)).km
         return distance_km <= radius_km
 
-    def find_city_by_coordinates(self, latitude: float, longitude: float):
+    @staticmethod
+    def find_city_by_coordinates(latitude: float, longitude: float):
         geolocator = Nominatim(user_agent="companies_app")
         location = geolocator.reverse(
             (latitude, longitude), exactly_one=True)
@@ -43,27 +81,3 @@ class OrganizationService:
         address = location.raw['address']
         city = address.get('city', '')
         return city
-
-    def get_by_coordinates(self, latitude: float, longitude: float, r: float):
-        city = self.find_city_by_coordinates(latitude, longitude)
-        print(city)
-        if not city:
-            return None
-        buildings = self.repo.get_buildings_by_city(city)
-        result_list = []
-        for b in buildings:
-            if self.is_within_radius(latitude, longitude, b.latitude, b.longitude, r):
-                building_dict = b.model_dump()
-                building_dict["organizations"] = (
-                    self.repo.get_organizations_by_building_address(b.city, b.street, b.house))
-                result_list.append(building_dict)
-        return result_list
-
-    def get_organization_by_id(self, organization_id: int):
-        return self.repo.get_organization_by_id(organization_id)
-
-    def get_organization_by_name(self, name: str):
-        return self.repo.get_organization_by_name(name)
-
-    def get_organizations_by_subactivities(self, activity: str):
-        return self.repo.find_organizations_by_activity(activity)
