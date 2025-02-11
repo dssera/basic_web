7d1e022e8e48cfcdeed9bce0adc0597dd32c35b0
dssera <adesser001@mail.ru>
2025-02-11 10:53:30 +0300

    refactor/use asyncpg url in async engine

--- app/db.py
+++ app/db.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import declarative_base
 from app.config import settings
 
 async_engine = create_async_engine(
-    url=settings.DATABASE_URL_psycopg,
+    url=settings.DATABASE_URL_asyncpg,
     echo=True
 )
 AsyncSessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=async_engine)
