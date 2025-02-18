fa74d3982d8b41a7c79b6a96f2db03bbe5a3bd1e
dssera <adesser001@mail.ru>
2025-02-18 12:47:47 +0300

    refactor/updated env files and added vars to config.py

--- app/config.py
+++ app/config.py
@@ -11,6 +11,9 @@ class Settings(BaseSettings):
     DB_NAME: str
     MODE: str
     API_SECRET_KEY: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     @property
     def DATABASE_URL_asyncpg(self):
